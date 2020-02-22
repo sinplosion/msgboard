44a439bd975bc5e25fb9be67b4fe69bab8100f80
sinplosion <ziggy.starzacher@gmail.com>
2020-02-22 03:18:19 +0200

    added automatic insertion of roles

--- application/role/models.py
+++ application/role/models.py
@@ -1,6 +1,6 @@
 from application import db
 from application.models import Base
-
+from sqlalchemy import event, DDL
 
 class Role(Base):
     name = db.Column(db.String(10), nullable=False)
@@ -13,4 +13,5 @@ class UserRole(Base):
 
 
     
-
+event.listen(Role.__table__,'after_create',
+            DDL(""" INSERT INTO Role (name) VALUES ('USER'), ('MOD'), ('ADMIN') """))
